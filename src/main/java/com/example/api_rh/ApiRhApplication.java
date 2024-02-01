6e96d5c66d7a2856ae0fdbd86ee510854637887a
Benjamin <hbenji@hotmail.com>
2024-02-01 15:26:09 +0100

    Init spring boot api rh project

---
+++ src/main/java/com/example/api_rh/ApiRhApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_rh/ApiRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
